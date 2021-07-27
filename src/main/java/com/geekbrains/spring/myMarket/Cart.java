bd5bba118218394beef718a90fe4461ae0dd65db
EvAlGodGood <Gooodfile@mail.ru>
2021-07-27 21:27:34 +0800

    Сделаны 3 из 4-х заданий. В четвертом нужно как-то с пользователя завязать? Не успеваю к занятию.

--- src/main/java/com/geekbrains/spring/myMarket/Cart.java
+++ src/main/java/com/geekbrains/spring/myMarket/Cart.java
@@ -1,1 +1,33 @@
-p
+package com.geekbrains.spring.myMarket;
+
+import com.geekbrains.spring.demo5.Item;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+@Component
+public class Cart {
+    private static List<Product> productCart;
+
+    @PostConstruct
+    public void init() {
+        productCart = new ArrayList<>(Arrays.asList());
+    }
+
+    public void addToCart(Product product) {
+        productCart.add(product);
+    }
+
+    public List<Product> findAll() {
+        return productCart;
+    }
+
+    public void removeById(Long id) {
+        productCart.remove(productCart.stream().filter(i -> i.getId().equals(id)).findFirst().get());
+        //нужно разобраться с лямбдами..
+    }
+
+}
